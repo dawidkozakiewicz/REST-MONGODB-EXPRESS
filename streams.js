c002179f5d876d82ad7d7f306c2351bf2d58ba49
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-07-30 20:38:07 +0200

    new methods

--- streams.js
+++ streams.js
@@ -1,8 +1,15 @@
 const fs = require('fs')
 
 const readStream = fs.createReadStream('./docs/hugefile.txt', { encoding: 'utf8' })
+const writeStream = fs.createWriteStream('./docs/hugeblog')
 
 readStream.on('data', (chunk) => {
     console.log('------------- NEW CHUNK------------')
     console.log(chunk)
+    writeStream.write('\nNEW CHUNK\n')
+    writeStream.write(chunk)
 })
+
+// PIPING
+
+// readStream.pipe(writeStream) - tu zrobi to samo co powyżej, ale prościej
