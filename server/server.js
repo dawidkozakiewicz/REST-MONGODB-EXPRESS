0f68cce543a8629cd8ccf5705080ab8bff8ebc00
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-08-22 20:56:35 +0200

    redirections

--- server/server.js
+++ server/server.js
@@ -30,6 +30,11 @@ const server = http.createServer((req, res) => {
             console.log(path);
             res.statusCode = 200;
             break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
         default:
             path += '404.html';
             console.log(path);
