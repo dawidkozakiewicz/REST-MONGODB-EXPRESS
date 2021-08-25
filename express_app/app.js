a51c2994a75e7ca06f32c6b82040e6290c2d44b5
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-08-25 20:56:41 +0200

    removing unnesessary modules and add gitignore

--- express_app/app.js
+++ express_app/app.js
@@ -3,12 +3,17 @@ const express = require('express')
 //express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs')
+// app.set('views', 'myViews')  - kiedy chcemy ustawić inny domyślny folder dla widoków nić
+
 //listen for requests
 app.listen(3000);
 app.get('/', (req, res) => {
 
     // res.send("<p>home page</p>")
-    res.sendFile('./views/index.html', { root: __dirname })
+    // res.sendFile('./views/index.html', { root: __dirname }) - tak było bez ejs'a
+    res.render('index');
 
 })
 
