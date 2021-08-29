19902e2da7976ccde1f711cddee2fd04e3080aba
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-08-29 20:51:56 +0200

    adding dynamic data into views

--- express_app/app.js
+++ express_app/app.js
@@ -13,15 +13,25 @@ app.get('/', (req, res) => {
 
     // res.send("<p>home page</p>")
     // res.sendFile('./views/index.html', { root: __dirname }) - tak było bez ejs'a
-    res.render('index');
+    res.render('index', { title: "Home" });
 
 })
 
 app.get('/about', (req, res) => {
 
     // res.send("<p>about page</p>")
-    res.sendFile('./views/about.html', { root: __dirname })
+    // res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about', { title: "About" });
 })
+
+app.get('/blogs/create', (req, res) => {
+
+    // res.send("<p>about page</p>")
+    // res.sendFile('./views/about.html', { root: __dirname })
+    res.render('create', { title: "Create a new blog" });
+})
+
+
 
 //redirects
 
@@ -33,5 +43,6 @@ app.get('/about-us', (req, res) => {
 app.use((req, res) => {  //musi być na samym dole
 
     // res.send("<p>about page</p>")
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    // res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404', { title: "404" });
 })
