46505bd055a7f677cfed2609004b76d447d31bd7
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-09-14 20:34:24 +0200

    reafactorisation

--- express_app_2.0/app.js
+++ express_app_2.0/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes')
 const { render } = require('ejs');
 
 
@@ -88,59 +88,7 @@ app.get('/about', (req, res) => {
     res.render('about', { title: "About" });
 })
 
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
 
-app.post('/blogs', (req, res) => {
-    console.log(req.body)
-    const blog = new Blog(req.body)
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs')
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-app.get('/blogs/create', (req, res) => { // ten route powinien być ponad routami z id, ale nie wiem czemu
-
-    // res.send("<p>about page</p>")
-    // res.sendFile('./views/about.html', { root: __dirname })
-    res.render('create', { title: "Create a new blog" });
-})
-
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id
-    Blog.findById(id)
-        .then(result => {
-            res.render('details', { blog: result, title: 'Blog Details' })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            res.json({ redirect: '/blogs' }) // musimy tak robić, ponieważ "DELETE" poszło jako AJAX z rzeglądarki a nie serwera
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-
-})
 
 
 
