250e401238bf5b227f9dfab49eff41ff32348236
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-09-18 20:56:36 +0200

    model, view, controller

--- express_app_2.0/controllers/blogController.js
+++ express_app_2.0/controllers/blogController.js
@@ -5,7 +5,7 @@ const Blog = require('../models/blog');
 const blog_index = (req, res) => {
     Blog.find().sort({ createdAt: -1 })
         .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result })
+            res.render('./blogs/index', { title: 'All Blogs', blogs: result })
         })
         .catch((err) => {
             console.log(err)
@@ -16,7 +16,36 @@ const blog_details = (req, res) => {
     const id = req.params.id
     Blog.findById(id)
         .then(result => {
-            res.render('details', { blog: result, title: 'Blog Details' })
+            res.render('./blogs/details', { blog: result, title: 'Blog Details' })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
+
+const blog_create_get = (req, res) => {
+    res.render('./blogs/create', { title: "Create a new blog" })
+}
+
+const blog_create_post = (req, res) => {
+    console.log(req.body)
+    const blog = new Blog(req.body)
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+}
+
+const blog_delete = (req, res) => {
+    const id = req.params.id
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            res.json({ redirect: '/blogs' }) // musimy tak robić, ponieważ "DELETE" poszło jako AJAX z rzeglądarki a nie serwera
         })
         .catch((err) => {
             console.log(err)
@@ -25,6 +54,9 @@ const blog_details = (req, res) => {
 
 module.exports = {
     blog_index,
-    blog_details
+    blog_details,
+    blog_create_get,
+    blog_create_post,
+    blog_delete
 }
 
--- express_app_2.0/routes/blogRoutes.js
+++ express_app_2.0/routes/blogRoutes.js
@@ -6,37 +6,13 @@ const router = express.Router()
 
 router.get('/', blogController.blog_index)
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    const blog = new Blog(req.body)
+router.post('/', blogController.blog_create_post)
 
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs')
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-router.get('/create', (req, res) => { // ten route powinien być ponad routami z id, ale nie wiem czemu
-
-    res.render('create', { title: "Create a new blog" });
-})
+router.get('/create', blogController.blog_create_get)
 
 
 router.get('/:id', blogController.blog_details)
 
-router.delete('/:id', (req, res) => {
-    const id = req.params.id
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            res.json({ redirect: '/blogs' }) // musimy tak robić, ponieważ "DELETE" poszło jako AJAX z rzeglądarki a nie serwera
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-
-})
+router.delete('/:id', blogController.blog_delete)
 
 module.exports = router
